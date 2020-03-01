0c2d9d3ffcbc9cb891f994bc135d29ffbb40236a
DrFran80 <frantraversaro@gmail.com>
2020-03-01 11:01:39 -0300

    Divided into classes

--- code/AmericanOption.py
+++ code/AmericanOption.py
@@ -2,7 +2,7 @@
 from Claim import Claim
 
 class AmericanOption( Claim ):
-    def __init__(self, strike):
+    def __init__(self, strike, maturity, underlying):
         self.strike = strike
     
     def payoff_ifexercise( self, spot_price ):
